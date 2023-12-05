210641e4fc5ab89706da4bfce73cf351e38aa95c
lunian <lunian@baidu.com>
2023-12-04 20:48:19 -0600

    test s3 select

--- s3filter/plan/cost_estimator.py
+++ s3filter/plan/cost_estimator.py
@@ -124,13 +124,9 @@ class CostEstimator:
 
     # These amounts vary by region, but for simplicity, let's assume it's a flat rate
 
-    # we didn't include s3 select, so don't calculate it
-    # COST_S3_DATA_RETURNED_PER_GB = 0.0007
-    COST_S3_DATA_RETURNED_PER_GB = 0
+    COST_S3_DATA_RETURNED_PER_GB = 0.0007
 
-    # we didn't include s3 select scan, so don't calculate it
-    # COST_S3_DATA_SCANNED_PER_GB = 0.002
-    COST_S3_DATA_SCANNED_PER_GB = 0
+    COST_S3_DATA_SCANNED_PER_GB = 0.002
     
     REQUEST_PRICE = 0.0004 / 1000.0
     DATA_TRANSFER_PRICE_PER_GB = 0.09
